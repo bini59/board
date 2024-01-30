80b9b20382f6210fc94a0804826e4c36203dabce
bini59 <myuni0234@gmail.com>
2024-01-30 15:54:03 +0900

    feat: DB 연결 method 생성

---
+++ server/repository/database.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'localhost',
+  user: 'root',
+  password: 'sql_server',
+  database: 'test_board',
+  port: '3306'
+}, (err)=>{
+  if(err) console.log(err);
+  else console.log('DB connected');
+});
+
+const query = (sql, params) => {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err) reject(err);
+
+      connection.query(sql, params, (err, rows) => {
+        if (err) reject(err);
+        resolve(rows);
+        connection.release();
+      });
+    });
+  });
+}
+
+module.exports = { query };
