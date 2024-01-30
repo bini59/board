9d77d0d97dca03948ab0148b47254c8a41a526c9
bini59 <myuni0234@gmail.com>
2024-01-30 13:47:39 +0900

    feat: add network to db container

--- DB/runMySQL.sh
+++ DB/runMySQL.sh
@@ -1,1 +1,1 @@
-docker run -d --name mysql -p 3306:3306 -v mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=sql_server mysql:8.3 --default-authentication-plugin=mysql_native_password
+docker run -d --name mysql -p 3306:3306 -v mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=sql_server --network=board mysql:8.3 --default-authentication-plugin=mysql_native_password
