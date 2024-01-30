287d3a890a82d64a54613011e9afbd8f6d43144a
bini59 <myuni0234@gmail.com>
2024-01-30 17:45:25 +0900

    feat: cors 설정

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var app = express();
 
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors()); // CORS 설정
 
 const apiList = ['post'];
 apiList.forEach(api => {
